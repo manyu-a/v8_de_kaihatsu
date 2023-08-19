bfd779ac08a2fa2941f6ed91aeb97a8eb02de912
sdaiki1029 <daiki1029@DESKTOP-O9D4ETJ>
2023-08-20 01:31:03 +0900

    remove-a.js

--- a.js
+++
@@ -1,9 +0,0 @@
-function showAlert() {
-  alert("今日も楽しく開発しよう！");
-}
-chrome.action.onClicked.addListener((tab) => {
- chrome.scripting.executeScript({
-		target: { tabId: tab.id },
-		function: showAlert
- });
-});
