adc0bbf09fc9f9fd7f5622bf5d028cd6e8b19ff2
manyu-a <manyumanyu110@gmail.com>
2023-08-19 19:26:56 +0900

    popupの追加

--- alert.js
+++ alert.js
@@ -1,9 +1,10 @@
 function showAlert() {
 	document.body.style.fontSize = "40px";
 }
+flag = 0
 chrome.action.onClicked.addListener((tab) => {
 	chrome.scripting.executeScript({
 		target: { tabId: tab.id },
-		function: showAlert
+        function: showAlert()
 	});
 });
