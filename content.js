d42bd484a6818489d6bb2d22d1963b09815d556a
manyu-a <manyumanyu110@gmail.com>
2023-08-20 13:46:08 +0900

    mani

--- content.js
+++ content.js
@@ -4,56 +4,60 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     alert('頑張ってクリックしてね');
 
     function getMousePosition(event) {
-      return { x: event.clientX, y: event.clientY };
+        return { x: event.clientX, y: event.clientY };
     }
 
     function applyTextEscapeEffect(element) {
-      const escapeDistance = 100;
-      let isEscaping = false;
+        const escapeDistance = 100;
+        let isEscaping = false;
 
-      element.addEventListener('mousemove', (event) => {
-        if (!isEscaping) {
-          isEscaping = true;
+        element.addEventListener('mousemove', (event) => {
+            if (!isEscaping) {
+                isEscaping = true;
 
-          const initialX = element.getBoundingClientRect().x;
-          const initialY = element.getBoundingClientRect().top;
+                const initialX = element.getBoundingClientRect().x;
+                const initialY = element.getBoundingClientRect().top;
 
-          const mousePosition = getMousePosition(event);
-          const initialMouseX = mousePosition.x;
-          const initialMouseY = mousePosition.y;
+                const mousePosition = getMousePosition(event);
+                const initialMouseX = mousePosition.x;
+                const initialMouseY = mousePosition.y;
 
-          const moveText = (event) => {
-            const mousePosition = getMousePosition(event);
-            const currentMouseX = mousePosition.x;
-            const currentMouseY = mousePosition.y;
+                const moveText = (event) => {
+                    const mousePosition = getMousePosition(event);
+                    const currentMouseX = mousePosition.x;
+                    const currentMouseY = mousePosition.y;
 
-            const deltaX = currentMouseX - initialMouseX;
-            const deltaY = currentMouseY - initialMouseY;
+                    const deltaX = currentMouseX - initialMouseX;
+                    const deltaY = currentMouseY - initialMouseY;
 
-            const escapeX = initialX + (deltaX * escapeDistance);
-            const escapeY = initialY + (deltaY * escapeDistance);
+                    const escapeX = initialX + (deltaX * escapeDistance);
+                    const escapeY = initialY + (deltaY * escapeDistance);
 
-            element.style.transition = 'transform 0.3s ease-in-out';
-            element.style.transform = `translate(${escapeX}px, ${escapeY}px)`;
-          };
+                    element.style.transition = 'transform 0.3s ease-in-out';
+                    element.style.transform = `translate(${escapeX}px, ${escapeY}px)`;
+                    // テキストカラーの変更
+                    const text = element.innerText;
+                    const new_text = text.replace(/\d+/g, '<newtext style="color: red;">$&</newtext>');
+                    element.innerHTML = new_text;
+                };
 
-          element.addEventListener('mousemove', moveText);
+                element.addEventListener('mousemove', moveText);
 
-          setTimeout(() => {
-            element.style.transition = 'transform 0.3s ease-in-out';
-            element.style.transform = `translate(0, 0)`;
-            element.removeEventListener('mousemove', moveText);
-            setTimeout(() => {
-              isEscaping = false;
-            }, 500);
-          }, 2000);
-        }
-      });
+                setTimeout(() => {
+                    element.style.transition = 'transform 0.3s ease-in-out';
+                    element.style.transform = `translate(0, 0)`;
+                    element.removeEventListener('mousemove', moveText);
+                    setTimeout(() => {
+                    isEscaping = false;
+                    }, 500);
+                }, 2000);
+            }
+        });
     }
 
     const textElements = document.querySelectorAll('p, span, h1, h2, h3, h4, h5, h6, a, li, td, th');
     textElements.forEach((element) => {
-      applyTextEscapeEffect(element);
+        applyTextEscapeEffect(element);
     });
   }
 });
