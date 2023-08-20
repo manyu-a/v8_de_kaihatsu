9a91ff60f6c60b7eda20f3a8f0f8cdade36dff73
manyu-a <manyumanyu110@gmail.com>
2023-08-20 14:33:02 +0900

    見た目変更とリロードボタン追加

--- content.js
+++ content.js
@@ -1,63 +1,67 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === 'performAction') {
-    // アラートを表示
-    alert('頑張ってクリックしてね');
+    if (request.action === 'performAction') {
+        // アラートを表示
+        alert('頑張ってクリックしてね');
 
-    function getMousePosition(event) {
-        return { x: event.clientX, y: event.clientY };
-    }
+        function getMousePosition(event) {
+            return { x: event.clientX, y: event.clientY };
+        }
 
-    function applyTextEscapeEffect(element) {
-        const escapeDistance = 100;
-        let isEscaping = false;
+        function applyTextEscapeEffect(element) {
+            const escapeDistance = 100;
+            let isEscaping = false;
 
-        element.addEventListener('mousemove', (event) => {
-            if (!isEscaping) {
-                isEscaping = true;
+            element.addEventListener('mousemove', (event) => {
+                if (!isEscaping) {
+                    isEscaping = true;
 
-                const initialX = element.getBoundingClientRect().x;
-                const initialY = element.getBoundingClientRect().top;
+                    const initialX = element.getBoundingClientRect().x;
+                    const initialY = element.getBoundingClientRect().top;
 
-                const mousePosition = getMousePosition(event);
-                const initialMouseX = mousePosition.x;
-                const initialMouseY = mousePosition.y;
-
-                const moveText = (event) => {
                     const mousePosition = getMousePosition(event);
-                    const currentMouseX = mousePosition.x;
-                    const currentMouseY = mousePosition.y;
+                    const initialMouseX = mousePosition.x;
+                    const initialMouseY = mousePosition.y;
 
-                    const deltaX = currentMouseX - initialMouseX;
-                    const deltaY = currentMouseY - initialMouseY;
+                    const moveText = (event) => {
+                        const mousePosition = getMousePosition(event);
+                        const currentMouseX = mousePosition.x;
+                        const currentMouseY = mousePosition.y;
 
-                    const escapeX = initialX + (deltaX * escapeDistance);
-                    const escapeY = initialY + (deltaY * escapeDistance);
+                        const deltaX = currentMouseX - initialMouseX;
+                        const deltaY = currentMouseY - initialMouseY;
 
-                    element.style.transition = 'transform 0.3s ease-in-out';
-                    element.style.transform = `translate(${escapeX}px, ${escapeY}px)`;
-                    // テキストカラーの変更
-                    const text = element.innerText;
-                    const new_text = text.replace(/\d+/g, '<newtext style="color: red;">$&</newtext>');
-                    element.innerHTML = new_text;
-                };
+                        const escapeX = initialX + (deltaX * escapeDistance);
+                        const escapeY = initialY + (deltaY * escapeDistance);
 
-                element.addEventListener('mousemove', moveText);
+                        element.style.transition = 'transform 0.3s ease-in-out';
+                        element.style.transform = `translate(${escapeX}px, ${escapeY}px)`;
+                        // テキストカラーの変更
+                        const text = element.innerText;
+                        const new_text = text.replace(/\d+/g, '<newtext style="color: red;">$&</newtext>');
+                        element.innerHTML = new_text;
+                    };
 
-                setTimeout(() => {
-                    element.style.transition = 'transform 0.3s ease-in-out';
-                    element.style.transform = `translate(0, 0)`;
-                    element.removeEventListener('mousemove', moveText);
+                    element.addEventListener('mousemove', moveText);
+
                     setTimeout(() => {
-                    isEscaping = false;
-                    }, 500);
-                }, 2000);
-            }
+                        element.style.transition = 'transform 0.3s ease-in-out';
+                        element.style.transform = `translate(0, 0)`;
+                        element.removeEventListener('mousemove', moveText);
+                        setTimeout(() => {
+                        isEscaping = false;
+                        }, 500);
+                    }, 2000);
+                }
+            });
+        }
+
+        const textElements = document.querySelectorAll('p, span, h1, h2, h3, h4, h5, h6, a, li, td, th');
+        textElements.forEach((element) => {
+            applyTextEscapeEffect(element);
         });
     }
 
-    const textElements = document.querySelectorAll('p, span, h1, h2, h3, h4, h5, h6, a, li, td, th');
-    textElements.forEach((element) => {
-        applyTextEscapeEffect(element);
-    });
-  }
+    if (request.action === "reloadAction"){
+        location.reload()
+    }
 });
--- popup.js
+++ popup.js
@@ -6,4 +6,11 @@ document.addEventListener('DOMContentLoaded', () => {
         chrome.tabs.sendMessage(tabs[0].id, { action: 'performAction' })
       })
     })
+    const reloadButton = document.getElementById('reloadButton')
+  
+    reloadButton.addEventListener('click', () => {
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, { action: 'reloadAction' })
+      })
+    })
   })
