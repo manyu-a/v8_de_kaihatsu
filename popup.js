eaa90fc13a112c7d1476463dc724b65631f724b9
kamei-34 <rakuzira.34.it@gmail.com>
2023-08-20 03:19:24 +0000

    first_try commit

--- popup.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const actionButton = document.getElementById('actionButton')
-  
-    actionButton.addEventListener('click', () => {
-      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id, { action: 'performAction' })
-      })
-    })
-  })
